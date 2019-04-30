66f7a67cd36d40dea1e686ea156024337df39744
Alba Tortosa <atortoslop@educacion.navarra.es>
2019-04-30 13:15:58 +0200

    Inicio del proyecto

---
+++ CalcularPeriodo/CalcularPeriodo/src/CalcularPeriodo.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+public class CalcularPeriodo {
+	
+	public static void main(String[] args) {
+		
+		//Creamos la fecha del descubrimiento de américa
+		LocalDate fecha = LocalDate.of(1492, Month.OCTOBER, 12);	 
+		// Mostramos cuánto tiempo ha pasado
+		calcularPeriodo("El descubrimiento de América", fecha);
+		
+		//4.1. Crea el evento "El nacimiento de Internet" el 21 de noviembre de 1969 
+		//y muestra cuánto tiempo ha pasado
+		
+		
+		//4.2. Crea el evento "La llegada a la Luna" el 20 de julio de 1969 
+		//y muestra cuánto tiempo ha pasado
+		
+		
+		
+	}
+	
+	public static void calcularPeriodo(String nombre, LocalDate fecha) {
+        
+        LocalDate fechaActual = LocalDate.now();
+        Period periodo = Period.between(fecha, fechaActual);
+        
+        // Cálculo de las diferencias
+        int anyos = periodo.getYears();
+        int meses = periodo.getMonths();
+        int dias = periodo.getDays();
+        
+        String texto = String.format(nombre + " ocurrió hace %d años, %d meses y %d días.", anyos, meses, dias);
+         
+        System.out.println(texto);
+    }
+ 
+}
