9c606f29b6e37a36cbb1e5b6c124fec736bec518
Alba Tortosa <atortoslop@educacion.navarra.es>
2019-04-30 13:22:49 +0200

    Clase Evento añadida

---
+++ CalcularPeriodo/CalcularPeriodo/src/Evento.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Evento {
+	
+	private String nombre;
+	private LocalDate fecha;
+	
+	public Evento(String nombre, LocalDate fecha) {
+		super();
+		this.nombre = nombre;
+		this.fecha = fecha;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+
+}
